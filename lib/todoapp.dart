a4ee4b95c3a6b00dcf8c75d8592d5e7d8d270fc8
megusfhmgaming <megus_fhmgaming@protonmail.com>
2021-04-25 17:37:26 +0700

    update

--- lib/todoapp.dart
+++ lib/todoapp.dart
@@ -12,7 +12,26 @@ class TodoApp extends StatelessWidget {
           children: [
             const Title(),
             const Input(),
+            const SizedBox(height: 8),
             const Toolbar(),
+            Consumer(
+              builder: (context, watch, child) {
+                final todoList = watch(todoListFilteredProvider);
+                return Column(
+                  children: [
+                    for (final todo in todoList) ...[
+                      ProviderScope(
+                        overrides: [
+                          todoItemProvider.overrideWithValue(todo),
+                        ],
+                        child: TodoItem(),
+                      ),
+                      SizedBox(height: 12),
+                    ],
+                  ],
+                );
+              },
+            ),
           ],
         ),
       ),
@@ -38,7 +57,20 @@ class Title extends StatelessWidget {
 }
 
 final _inputController = TextEditingController();
-final todoListProvider = StateNotifierProvider((_) => TodoList([]));
+final todoListProvider =
+    StateNotifierProvider<TodoList, List<Todo>>((_) => TodoList([]));
+final todoListFilteredProvider = Provider<List<Todo>>((ref) {
+  final filter = ref.watch(toolbarFilterProvider);
+  final todoList = ref.watch(todoListProvider);
+  switch (filter.state) {
+    case TodoFilter.Active:
+      return todoList.where((element) => !element.completed).toList();
+    case TodoFilter.Completed:
+      return todoList.where((element) => element.completed).toList();
+    default:
+      return todoList.toList();
+  }
+});
 
 class Input extends StatelessWidget {
   const Input({Key? key}) : super(key: key);
@@ -64,7 +96,7 @@ class Todo {
   bool completed;
 }
 
-class TodoList extends StateNotifier {
+class TodoList extends StateNotifier<List<Todo>> {
   TodoList(List<Todo> initData) : super(initData);
   void addTodo(String desc) {
     state = [
@@ -72,22 +104,99 @@ class TodoList extends StateNotifier {
       Todo(id: UniqueKey().toString(), description: desc),
     ];
   }
+
+  void toggleTodo(String id) {
+    state = [
+      for (final todo in state)
+        todo.id == id
+            ? Todo(
+                id: id,
+                description: todo.description,
+                completed: !todo.completed,
+              )
+            : todo,
+    ];
+  }
 }
 
-enum TodoFilter { All, Active, COmpleted }
+enum TodoFilter { All, Active, Completed }
+final toolbarFilterProvider = StateProvider((_) => TodoFilter.All);
+
 class Toolbar extends StatelessWidget {
   const Toolbar({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        TextButton(
-          onPressed: () {},
-          child: Text('All'),
-        ),
-      ],
-    );
+    return Consumer(builder: (context, watch, child) {
+      final selectFilter = watch(toolbarFilterProvider).state;
+
+      TextStyle txtStyle(TodoFilter filter) {
+        return filter == selectFilter
+            ? TextStyle(
+                fontSize: 18,
+                color: Colors.cyan,
+              )
+            : TextStyle(
+                fontSize: 18,
+                color: Colors.black38,
+              );
+      }
+
+      return Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          TextButton(
+            onPressed: () {
+              context.read(toolbarFilterProvider).state = TodoFilter.All;
+            },
+            child: Text(
+              'All',
+              style: txtStyle(TodoFilter.All),
+            ),
+          ),
+          TextButton(
+            onPressed: () {
+              context.read(toolbarFilterProvider).state = TodoFilter.Active;
+            },
+            child: Text(
+              'Active',
+              style: txtStyle(TodoFilter.Active),
+            ),
+          ),
+          TextButton(
+            onPressed: () {
+              context.read(toolbarFilterProvider).state = TodoFilter.Completed;
+            },
+            child: Text(
+              'Completed',
+              style: txtStyle(TodoFilter.Completed),
+            ),
+          ),
+        ],
+      );
+    });
   }
 }
 
-final toolbarFilterProvider = StateProvider((_) => TodoFilter.All);
+final todoItemProvider = ScopedProvider<Todo>(null);
+
+class TodoItem extends StatelessWidget {
+  const TodoItem();
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      elevation: 2,
+      child: Consumer(builder: (context, watch, child) {
+        final todo = watch(todoItemProvider);
+        return ListTile(
+          leading: Checkbox(
+            onChanged: (bool? value) {
+              context.read(todoListProvider.notifier).toggleTodo(todo.id);
+            },
+            value: todo.completed,
+          ),
+          title: Text('${todo.description}'),
+        );
+      }),
+    );
+  }
+}
